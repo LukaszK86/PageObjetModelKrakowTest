7864af39062c61c1da2b12d592435a88bad4cb75
Lukasz Kaczmarczyk <lucas.kaczmarczyk@gmail.com>
2017-10-10 15:00:02 +0200

    First commit

---
+++ src/main/java/MainPage.java
@@ -0,0 +1,30 @@
+import org.junit.jupiter.api.Assertions;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class MainPage {
+
+    WebDriver driver;
+
+    By searchBox = By.id("lst-ib");
+    By searchButton = By.name("btnK");
+    By word = By.id("rhs_title");
+
+    public MainPage (WebDriver driver){
+this.driver = driver;
+    }
+
+    public void typeSearchBox (){
+        driver.findElement(searchBox).sendKeys("Kraków");
+    }
+
+    public void clickOnLoginButton(){
+        driver.findElement(searchButton).click();
+    }
+
+    public void verifyWordKrakow (){
+        String word = driver.findElement(By.id("rhs_title")).getText();
+        Assertions.assertEquals("Kraków", word);
+    }
+
+}
---
+++ src/main/java/VerificationKrakowWord.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class VerificationKrakowWord {
+
+
+       @Test
+       public void verifyKrakowWord () {
+           WebDriver driver = new ChromeDriver();
+           driver.get("http://google.com");
+
+           MainPage google = new MainPage(driver); //related with Mainpage
+
+           google.typeSearchBox();
+           google.clickOnLoginButton();
+           google.verifyWordKrakow();
+    }
+}
