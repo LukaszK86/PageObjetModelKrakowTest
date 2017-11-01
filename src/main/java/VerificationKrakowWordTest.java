c752e1fee1d03abf420eee26b94367c0a531cce8
Lukasz Kaczmarczyk <lucas.kaczmarczyk@gmail.com>
2017-11-01 15:33:26 +0100

    Name changed Test

--- src/main/java/VerificationKrakowWordTest.java
+++ src/main/java/VerificationKrakowWordTest.java
@@ -2,7 +2,7 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class VerificationKrakowWord {
+public class VerificationKrakowWordTest {
 
 
        @Test
